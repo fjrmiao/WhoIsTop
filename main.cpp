a03519ad228497d9d06407a7a11ac460f5e37e04
fjrmiao <48702502+fjrmiao@users.noreply.github.com>
2020-06-01 18:57:02 +0800

    v0.2
    unfinished

--- main.cpp
+++ main.cpp
@@ -2,115 +2,49 @@
 #include <fstream>
 #include <windows.h>
 
+#include "public.h"
+#include "whoistop.h"
+#include "whentodie.h"
 using namespace std;
 
-string LowCase(string x) {
-	unsigned int xlen = x.length();
-	for (unsigned int i = 0; i < xlen; i++) {
-		if (x[i] > 'A' && x[i] < 'Z') {
-			x[i] += 32;
-		}
-	}
-	return x;
+whoistop *WIT;
+whentodie* WTD;
+
+void CALLBACK TimerProcWIT(	HWND hWnd,	UINT nMsg,	UINT_PTR dwUser,	DWORD dwTime) {
+	WIT->scan();
 }
 
-string LowCase(char *y) {
-	string x = y;
-	unsigned int xlen = x.length();
-	for (unsigned int i = 0; i < xlen; i++) {
-		if (x[i] > 'A' && x[i] < 'Z') {
-			x[i] += 32;
-		}
-	}
-	return x;
-}
+void CALLBACK TimerProcWTD(HWND hWnd, UINT nMsg, UINT_PTR dwUser, DWORD dwTime) {
 
-int LoadStrFile(string szFileName,string szData[]) {
-	cout << "load " <<szFileName  << ":" << endl;
-	ifstream srcFile(szFileName, ios::in);
-	int n = 0;
-	while (srcFile >> szData[n]) {
-		n++;
-		if (n > 199) {
-			break;
-		}
-		cout << szData[n-1] << endl;
-	}
-		
-	return n;
+	SYSTEMTIME sys;
+	GetLocalTime(&sys);
+	//cout << sys.wYear << "//"  << sys.wMonth <<  "//" << sys.wDay  << endl;
+
 }
 
 int main(int argc, char** argv) {
 	//HWND hDesktop;
-	HWND hCurrentTop;
-	HWND hOldTop=0;
-	HWND hMe;
-	string szAllowInTop[200];
-	TCHAR szCurrentPath[255] = { 0 };
-	bool bHideConsole = true;
 
-	GetCurrentDirectory( 200, szCurrentPath);
-	DWORD NoHideAttr = GetFileAttributes("NoHide");
-	hMe = GetConsoleWindow();
-	if (NoHideAttr != 0xffffffff)
-	{
-		ShowWindow(hMe, SW_SHOW);
-	}
-	else{
-		ShowWindow(hMe, SW_HIDE);
+	
+	HideConsoleWindow();
+	
+	WIT = new whoistop();
+	SetTimer(NULL, 1000, 200, TimerProcWIT);
 
-	}
-	printf("CurrentPath = %s \t NoHide Attrib = %d\n", szCurrentPath, NoHideAttr);
+	WTD = new whentodie();
+	SetTimer(NULL, 2000, 1000, TimerProcWTD);
 
-	int AllowInTopCount = LoadStrFile("AllowInTop.txt", szAllowInTop);
-
-	bool bWantLock = false;
-
+	
 	cout << "Who is Top:" << endl;
+	
 	while(1){
-		//hDesktop = GetDesktopWindow();hCurrentTop = GetTopWindow(hDesktop);
-		hCurrentTop = GetForegroundWindow();
-		//hCurrentTop = GetTopWindow(NULL);
-		
-		if(hCurrentTop!=hOldTop){
-			DWORD pidCurrentTop;
-			TCHAR szFileName[255] = { 0 };
-
-			
-			bool  bIsAllowInTop = false;
-
-			hOldTop=hCurrentTop;
-			GetWindowThreadProcessId(hCurrentTop,&pidCurrentTop);
-
-			HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
-					               0, pidCurrentTop);
-				if(handle)
-				{
-					DWORD maxsize = 255;
-					QueryFullProcessImageName(handle, 0, szFileName, &maxsize);
-					for (int i = 0; i < AllowInTopCount; i++) {
-						
-						if (LowCase(szAllowInTop[i]) == LowCase(szFileName)) {
-							bIsAllowInTop = true;
-							break;
-						}
-					}
-					
-					if (bWantLock == 1 && bIsAllowInTop == 0) {
-						LockWorkStation();
-						printf("!!!!!!!!!!!!!!!Lock>>>");
-						bWantLock = 0;
-					}
-					if (bWantLock == 0 && bIsAllowInTop == 1) {
-						bWantLock = 1;
-					}
-					CloseHandle(handle);
+		MSG   msg;
+		if(GetMessage(&msg, NULL, 0, 0)){
+			 if (msg.message == WM_TIMER){
+				             DispatchMessage(&msg);
 				}
-
-			printf("HWND = %p\t PID = %d\t FileName = %s\t AllowInTop = %d \n",hCurrentTop,pidCurrentTop,szFileName, bIsAllowInTop);
-			
-			
 		}
+		
 	}
 	return 0;
 }
